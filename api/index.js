b586f375f1a1fb7f13794773f0162d464ca1b25a
Gnanasekarr <gnanasekarrajarathinam@gmail.com>
2024-05-17 20:06:29 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+// mongoose.connect("mongodb+srv://mern:Mern@mern.azu3gur.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern");
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log("connect t mongodb");
+}).catch((err) =>{
+    console.log(err);
+});
 
 const app = express();
 
