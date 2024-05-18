43f8b2e95ffbc8923912f431f33a67b6d4ba90a5
Gnanasekarr <gnanasekarrajarathinam@gmail.com>
2024-05-18 11:45:17 +0530

    create aignup api route

--- api/index.js
+++ api/index.js
@@ -2,20 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 // mongoose.connect("mongodb+srv://mern:Mern@mern.azu3gur.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern");
 
 mongoose.connect(process.env.MONGO).then(() =>{
-    console.log("connect t mongodb");
+    console.log("connect t0 mongodb");
 }).catch((err) =>{
     console.log(err);
 });
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000,() =>{
     console.log("server listening 3000");
 });
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
