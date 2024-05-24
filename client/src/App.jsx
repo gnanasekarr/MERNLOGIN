9a05f92b7c9e50c9fc2d9c0a1b8c95bea98fe022
Gnanasekarr <gnanasekarrajarathinam@gmail.com>
2024-05-24 13:22:08 +0530

    update header component and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import About from './pages/About.jsx';
 import SignUp from './pages/SignUp.jsx';
 import SignIn from './pages/SignIn.jsx';
 import Header from './components/Header.jsx'
-
+import PrivateRouter from './components/PrivateRouter.jsx'
 export default function App() {
   return <BrowserRouter>
   {/*header */}
   <Header />
   <Routes>
   <Route path="/" element={<Home/>}/>
-  <Route path="/profile" element={<Profile/>}/>
   <Route path="/about" element={<About/>}/>
   <Route path="/sign-up" element={<SignUp/>}/>
-    <Route path="/sign-in" element={<SignIn/>}/>
+  <Route path="/sign-in" element={<SignIn/>}/>
+  <Route element={<PrivateRouter/>}>
+  <Route path="/profile" element={<Profile/>}/>
+  </Route>
   </Routes>
   </BrowserRouter>
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom"
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  const {currentUser} = useSelector((state) => state.user);
+  console.log(currentUser);
   return (
     <div className='bg-slate-200'>
         <div className=' flex justify-between items-center 
@@ -15,8 +18,14 @@ export default function Header() {
                 <Link to='/about'>
                 <li>About</li>
                 </Link>
-                <Link to='/sign-in'>
-                <li>Sign In</li>
+                <Link to='/profile'>
+                  {currentUser ?
+                  (
+                    <img src={currentUser.profilePictures } alt='profile'
+                    className="h-7 w-7 rounded-full object-cover"/>
+                  ):(
+                  <li>Sign In</li>
+                  )}
                 </Link>
              </ul>
         </div>
